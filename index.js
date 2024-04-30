19b269b0e7b076b7272738c5a9482a4356a476df
Md <mdmonis128@gmail.com>
2024-04-30 13:00:09 +0530

    index.js Updated

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-// import App from './App';
-import AppPro from './AppPro';
+import App from './App';
+// import AppPro from './AppPro';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => AppPro);
+AppRegistry.registerComponent(appName, () => App);
